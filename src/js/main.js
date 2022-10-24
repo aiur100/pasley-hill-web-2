9d63fc73b7478c2c584735e32242c707cb461cb9
Christopher Hill <chill@redlineadvantage.com>
2022-10-24 16:42:27 -0400

    Transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
 'use strict';
-const mobileMenu = document.getElementById('mobile-menu');
 const mCloseMenuBtn = document.getElementById('close-button');
 const mOpenMenuBtn = document.getElementById('open-menu-button');
+const mGetQuoteBtn = document.getElementById('get-a-quote');
+const deskMenu = document.getElementById('desk-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const hiddenElements = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show-it');
+        }else{
+            entry.target.classList.remove('show-it');
+        }
+    })
+})
+
+hiddenElements.forEach((el) => observer.observe(el));
 
 mCloseMenuBtn.onclick = () => {
     mobileMenu.classList.add('invisible');
@@ -9,5 +25,17 @@ mCloseMenuBtn.onclick = () => {
 mOpenMenuBtn.onclick = () => {
     mobileMenu.classList.remove('invisible');
 }
+
+function scrollToClick(e){
+    e.preventDefault();
+    const elmString = e.target?.dataset?.elem;
+    const element = document.getElementById(elmString);
+    if(!element) return (console.warn('Element not scrollable'));
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+deskMenu.onclick = scrollToClick;
+mobileMenu.onclick = scrollToClick;
+mGetQuoteBtn.onclick = scrollToClick;
 
 console.log('hello world');
